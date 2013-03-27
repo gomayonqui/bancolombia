7f6e2b45c1ad9581cd65a049ac02878799004be5
Jean Pierre Guarin <escribimepues@gmail.com>
2013-03-27 18:44:31 -0500

    Basic balance functionality

--- bancolombia.rb
+++ bancolombia.rb
@@ -3,13 +3,47 @@ require "yaml"
 require 'highline/import'
 
 begin
-  puts "checking : config.yml"
+  puts "Config file detected"
   yml = YAML::load(File.open('config.yml'))
   username = yml["username"]
   password = yml["password"]
 rescue Exception
-  puts "failed to read config: #{$!}"
-  username = ask("Enter bancolombia username: ")
-  password = ask("Enter bancolombia password: ") { |q| q.echo = "*" }
+  puts "No config file to read."
+  username = ask("Enter your bancolombia username: ")
+  password = ask("Enter your 4 digits bancolombia password: ") { |q| q.echo = "*" }
 end
 
+##Login username page
+agent = Mechanize.new
+agent.get("https://bancolombia.olb.todo1.com/olb/Init")
+userLoginPage = agent.get("https://bancolombia.olb.todo1.com/olb/Login")
+userLoginPage.form_with(:name => "authenticationForm") do |f|
+  f.userId = username
+end.click_button
+
+##Redirect to password page
+passwordPage = agent.get("https://bancolombia.olb.todo1.com/olb/GetUserProfile")
+passwordPageHtml = passwordPage.body
+##Insert \r for correct regex on password scripts
+passwordPageHtml.gsub!(/document.getElementById/, "\r\n\t document.getElementById")
+enc_password = ""
+password.to_s.split('').each do |n|
+  passwordPageHtml.match(/\'td_#{n}\'\)\.addEventListener\(\'click\'\,\sfunction\(\)\{\S*\(\"(.*)\"\)\;\}/);
+  enc_password << $1
+end
+##Capture secret hidden field
+passwordPageHtml.match(/'PASSWORD\':\'(.*)\'/)
+secretHiddenField = $1
+
+##Post encripted password and secret hidden field
+passwordPage.form_with(:name => "authenticationForm") do |f|
+  f.userId = "0"
+  f.password = enc_password
+  f.add_field!(secretHiddenField, value = enc_password)
+end.click_button
+
+##Get Balance
+agent.get("https://bancolombia.olb.todo1.com/olb/Authentication")
+balancePage = agent.get("/olb/BeginInitializeActionFromCM?from=pageTabs")
+balance = balancePage.search(".contentTotalNegrita").last.children.text
+puts "Your actual balance is: $#{balance}"
